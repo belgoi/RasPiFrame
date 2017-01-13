faaddc8b937d9a04354fb443835dc433ff8449b0
David Hinchliffe <belgoi@gmail.com>
2017-01-12 19:50:12 -0600

    Mainly bug fixes   --added validation when reading in time_to_sleep and time_to_wake   --added an option to preserve the aspect ratios of the slideshow images

--- src/raspiframe/PhotoFrameController.java
+++ src/raspiframe/PhotoFrameController.java
@@ -93,6 +93,7 @@ public class PhotoFrameController implements Initializable
         this();
         this.model=model;
         this.weatherModel=weatherModel;
+        //test
     }
   
     @Override
